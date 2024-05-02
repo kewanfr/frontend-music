5bed7f32dcd5ea17184a25adf199408ab5f6b306
Kéwan Bernier <mail@kewan.fr>
2024-05-03 01:14:55 +0200

    feat: ✨Others icons + LoadingIcon and chevron right

---
+++ src/components/icons/LoadingIcon.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="animate-spin w-6 h-6 border-t-4 border-blue-500 border-solid rounded-full"></div>
+</template>
+
+<style>
+.animate-spin {
+    animation: spin 1s linear infinite;
+}
+</style>
