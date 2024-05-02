f8a9fc7b5c3c67100bbdd6ed5c37f5f02e219702
Kéwan Bernier <mail@kewan.fr>
2024-05-03 01:16:16 +0200

    feat: ✨Cover Image component

---
+++ src/components/music/CoverImage.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src="src" :alt="alt" width="48" height="48" :class="classNames" loading="lazy" decoding="async">
+</template>
+
+<script setup>
+const props = defineProps({
+    src: String,
+    alt: String,
+    type: String
+});
+
+// class for rounded corners
+const roundedClass = props.type == "artist" ? "rounded-full" : "rounded-md";
+const classNames = "w-12 h-12 " + roundedClass;
+
+</script>
