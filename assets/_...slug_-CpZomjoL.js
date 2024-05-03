814d2de87fcc24a92dfaa8510c0d2a7ef3d7e476
kewanfr <kewanfr@users.noreply.github.com>
2024-05-03 14:58:13 +0000

    deploy: e0f5f986686990d577bedd6b59eaabae53ca894d

---
+++ assets/_...slug_-CpZomjoL.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,a as t}from"./index-EW2u0gKn.js";const _={},c={class:"about"},a=t("h1",null,"This is an about page",-1),n=[a];function r(i,d){return s(),o("div",c,n)}const u=e(_,[["render",r]]);export{u as default};
