3347bea43c7a546d7174eec2943f4b61dd2f818c
Kéwan Bernier <mail@kewan.fr>
2024-05-03 15:52:54 +0200

    Auto router

---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <div class="flex flex-col items-center space-y-4 mt-5">Télécharger</div>
+    <div class="divide-y divide-muted">
+      <div class="flex flex-col items-center space-y-4 mt-5">
+
+        <SearchBar />
+        <ResultsList />
+
+        <LoadingSpin :isLoading="isLoading" />
+
+        <div id="toasts" class="fixed bottom-0 right-0 p-4 z-20">
+        </div>
+
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import LoadingSpin from '@/components/LoadingSpin.vue';
+import ResultsList from '@/components/music/ResultsList.vue';
+
+import { useMusicStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import SearchBar from '../components/SearchBar.vue';
+
+const musicStore = useMusicStore();
+
+const { isLoading } = storeToRefs(musicStore);
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import DownloadView from "../views/DownloadView.vue";
+import { createRouter, createWebHistory } from "vue-router/auto";
+// import DownloadView from "../views/DownloadView.vue";
 
 import { useAuthStore } from "@/stores";
-import LoginView from "@/views/LoginView.vue";
-import QueueView from "@/views/QueueView.vue";
-import LibraryView from "@/views/LibraryView.vue";
-import LyricsView from "@/views/LyricsView.vue";
+// import LoginView from "@/views/LoginView.vue";
+// import QueueView from "@/views/QueueView.vue";
+// import LibraryView from "@/views/LibraryView.vue";
+// import LyricsView from "@/views/LyricsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: "bg-gray-300",
-  routes: [
-    { path: "/", component: DownloadView },
-    { path: "/login", component: LoginView },
-    { path: "/logout", component: LoginView },
-    { path: "/queue", component: QueueView },
-    { path: "/lyrics", component: LyricsView },
-    { path: "/library", component: LibraryView },
+  // routes: [
+  //   { path: "/", component: DownloadView },
+  //   { path: "/login", component: LoginView },
+  //   { path: "/logout", component: LoginView },
+  //   { path: "/queue", component: QueueView },
+  //   { path: "/lyrics", component: LyricsView },
+  //   { path: "/library", component: LibraryView },
 
-    // otherwise redirect to home
-    { path: "/:pathMatch(.*)*", redirect: "/" },
-  ],
+  //   // otherwise redirect to home
+  //   { path: "/:pathMatch(.*)*", redirect: "/" },
+  // ],
 });
 
 router.beforeEach(async (to) => {
@@ -43,5 +43,4 @@ router.beforeEach(async (to) => {
   }
 });
 
-
-export default router
+export default router;
