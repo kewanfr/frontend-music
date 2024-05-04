258979be27135bccfa1e1e4ced679ebb31df384e
kewanfr <kewanfr@users.noreply.github.com>
2024-05-04 03:49:47 +0000

    deploy: 6097a7bc3c6b9ceb73f51724403027995ebbdaa5

---
+++ assets/_...slug_-DRgBqco0.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as _,a as e}from"./index-C9BvhmM7.js";const c={},t={class:"error"},r=e("h1",null,"Erreur 404",-1),n=e("p",null,"La page que vous cherchez n'existe pas.",-1),a=[r,n];function l(d,i){return o(),_("div",t,a)}const h=s(c,[["render",l]]);export{h as default};
