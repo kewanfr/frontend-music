b90f6a6f41fcd0c1d2c114a5703aabb8894932c9
Kéwan Bernier <mail@kewan.fr>
2024-05-09 20:34:05 +0200

    feat: ✨searchBar, flag to define if exec search on input, and props searchFunction

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="w-full max-w-xl flex border rounded overflow-hidden mb-2">
         <input placeholder="Recherchez une musique..." class="p-2 flex-grow text-black" type="text" id="search-input"
-            v-model="query" @keydown.enter="search" />
-        <button class="bg-blue-500 text-white p-2 rounded-r hover:bg-blue-400" id="search-button" @click="search">
+            @keydown.enter="search" @input="inputSearch" v-model="query" />
+        <button class="bg-blue-500 text-white p-2 rounded-r hover:bg-blue-400" id="search-button" @click="search"
+            v-if="searchButton">
             <SearchIcon />
         </button>
     </div>
@@ -11,21 +12,28 @@
 
 <script setup>
 import SearchIcon from '@/components/icons/SearchIcon.vue';
-import { useMusicStore } from '@/stores';
-import { onMounted, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { ref, onMounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
-
-const musicStore = useMusicStore();
+const props = defineProps({
+    searchAction: Function,
+    searchButton: Boolean,
+    searchOnInput: Boolean
+});
 const router = useRouter();
 const route = useRoute();
 
 const query = ref(route.query.q || '');
 
-
-const search = async () => {
+const search = () => {
     router.push({ query: { q: query.value } });
-    await musicStore.search(query.value);
+    props.searchAction(query.value);
+}
+
+const inputSearch = () => {
+    if (props.searchOnInput) {
+        search();
+    }
 }
 
 onMounted(() => {
@@ -33,5 +41,6 @@ onMounted(() => {
         search();
     }
 })
+
 
 </script>
