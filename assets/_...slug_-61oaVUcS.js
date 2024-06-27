2c9fb1bc48e753d5e6a5705af0f026109994924e
kewanfr <kewanfr@users.noreply.github.com>
2024-06-27 01:18:40 +0000

    deploy: 32ca697e9d57b2377159cb8015d9329a10bdfbed

--- assets/_...slug_-61oaVUcS.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o,c as _,a as e}from"./index-B4v70nYK.js";const c={},t={class:"error"},r=e("h1",null,"Erreur 404",-1),n=e("p",null,"La page que vous cherchez n'existe pas.",-1),a=[r,n];function l(d,i){return o(),_("div",t,a)}const h=s(c,[["render",l]]);export{h as default};
